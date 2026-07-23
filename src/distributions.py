2283c850dcade1c6ea16e12461d267abe78af02e
Lucas <lucasbertin2007@gmail.com>
2026-07-23 20:40:55 +0200

    petit avancement

--- src/distributions.py
+++ src/distributions.py
@@ -1,4 +1,28 @@
-import math, random
+from math import exp, factorial 
+import random
 
-def sim_event(landa) :
+cumul = 0
+
+def poisson(k, landa) :
+    return (landa**k * exp(-landa)) / factorial(k) # P(N = k)
     
+def sim_events(landa) :
+    u = random.random() # between 0 and 1
+    cumul = 0
+    k = 0
+    
+    while True:
+        p = poisson(k, lam)
+        cumul += p
+        
+        if u <= cumul:
+            return k
+        
+        k += 1
+
+# now, if we use a range of a certain number and do print(sim_events(landa)) in the range, we will have a "number" of answers to which the avg will be landa (the purpose of itself)
+
+def lognormal()
+
+def cost_sim(mu, sigma) :
+     
---
+++ src/simulation.py
@@ -0,0 +1,2 @@
+# We will use a lognormal distribution because this law is : always positive, asymetric, and can simulate an occasionnaly big loss
+
