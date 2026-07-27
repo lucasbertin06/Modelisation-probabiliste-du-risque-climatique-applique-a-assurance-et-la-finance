16facb85f6009284fcc92650e1e39fc480406974
Lucas <lucasbertin2007@gmail.com>
2026-07-27 14:43:11 +0200

    changement distrib, mise en place loi lognormale dans risk_model

--- src/climat_risk.py
+++ src/climat_risk.py
@@ -1,19 +1,22 @@
 FLOOD = {
     "name" = "flood",
     "frequence" = 4,
-    "avg cost" = 10000
+    "avg cost" = 10000,
+    "sigma" = 0.4
 }
 
 FIRE = {
     "name" = "fire",
     "frequence" = 3,
-    "avg cost" = 30000
+    "avg cost" = 30000,
+    "sigma" = 0.65
 }
 
 STORM = {
     "name" = "storm",
     "frequence" = 1,
-    "avg cost" = 50000
+    "avg cost" = 50000,
+    "sigma" = 0.8
 }
 
 HAZARDS = [FLOOD, FIRE, STORM]
--- src/risk_model.py
+++ src/risk_model.py
@@ -1,4 +1,17 @@
+from math import log
+from distributions import sim_events, cost_sim
+
 # We will use this .py to do a ONE year simulation before doing this for a bigger number
+
+def mu_from_avg_cost(avg_cost, sigma):
+    """ 
+    For a lognormal distribution, E(X) = exp(mu + sigma^2 / 2).
+    So mu = ln(avg_cost) - sigma^2 / 2.
+ 
+    This lets us define risks by their intuitive 'average cost' in
+    climat_risk.py
+    """
+    return log(avg_cost) - (sigma ** 2) / 2
 
 def total_loss() :
     N = sim_events(landa)
