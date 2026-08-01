5d9e605453f3d15214076f3fd21b69fe8f41cbb4
Lucas <lucasbertin2007@gmail.com>
2026-08-01 16:17:02 +0200

    correction erreur clim_risk et initialisation clim_scenario

--- src/climat_risk.py
+++ src/climat_risk.py
@@ -1,22 +1,22 @@
 FLOOD = {
-    "name" = "flood",
-    "frequence" = 4,
-    "avg cost" = 10000,
-    "sigma" = 0.4
+    "name" : "flood",
+    "frequence" : 4,
+    "avg cost" : 10000,
+    "sigma" : 0.4
 }
 
 FIRE = {
-    "name" = "fire",
-    "frequence" = 3,
-    "avg cost" = 30000,
-    "sigma" = 0.65
+    "name" : "fire",
+    "frequence" : 3,
+    "avg cost" : 30000,
+    "sigma" : 0.65
 }
 
 STORM = {
-    "name" = "storm",
-    "frequence" = 1,
-    "avg cost" = 50000,
-    "sigma" = 0.8
+    "name" : "storm",
+    "frequence" : 1,
+    "avg cost" : 50000,
+    "sigma" : 0.8
 }
 
 HAZARDS = [FLOOD, FIRE, STORM]
---
+++ src/climatic_scenario.py
@@ -0,0 +1,22 @@
+# We will use this .py to create differents level of severity of thr event
+
+CURRENT = {
+    "name" : "Current climate"
+    "frequency" : 1,
+    "severity" : 1
+}
+
+MODERATE = {
+    "name" : "Moderate warning"
+    "frequency" : 1.3,
+    "severity" : 1.25
+}
+
+EXTREME = {
+    "name" : "Extreme warning"
+    "frequency" : 1.8,
+    "severity" : 1.6
+}
+
+SCENARIOS = [CURRENT, MODERATE, EXTREME]
+
