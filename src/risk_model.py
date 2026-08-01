f4a3ef8937dfd5bdd152600aa6327a172c25d861
Lucas <lucasbertin2007@gmail.com>
2026-08-01 16:53:26 +0200

    changement de risk_model car changement dans distribution.py

--- src/risk_model.py
+++ src/risk_model.py
@@ -3,24 +3,13 @@ from distributions import sim_events, cost_sim
 
 # We will use this .py to do a ONE year simulation before doing this for a bigger number
 
-def mu_from_avg_cost(avg_cost, sigma):
-    """ 
-    For a lognormal distribution, E(X) = exp(mu + sigma^2 / 2).
-    So mu = ln(avg_cost) - sigma^2 / 2.
- 
-    This lets us define risks by their intuitive 'average cost' in
-    climat_risk.py
-    """
-    return log(avg_cost) - (sigma ** 2) / 2
-
 def total_loss(risk) :
     N = sim_events(risk["frequence"])
-    sigma = risk["sigma"]
-    mu = mu_from_avg_cost(risk["avg_cost"], sigma)
 
     total = 0
-    for i in range(0, N) :
-        total += cost_sim(mu, sigma)
+
+    for i in range(N) :
+        total += cost_sim(risk["avg_cost"], risk["sigma"])
 
     return total
 
